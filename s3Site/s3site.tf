3bc037333ea87dcd365fde107228afccb46288d5
Jan Dockx <Jan_Dockx@peopleware.be>
2016-06-30 16:31:10 +0200

    Still fighting to get this to work. Another attempt.

--- s3Site/s3site.tf
+++ s3Site/s3site.tf
@@ -61,7 +61,7 @@ resource "aws_route53_record" "DOMAIN_NAME" {
   type = "A"
 
   alias {
-    name = "${aws_s3_bucket.BUCKET.website_endpoint}"
+    name = "${aws_s3_bucket.BUCKET.website_domain}"
     zone_id = "${aws_s3_bucket.BUCKET.hosted_zone_id}"
     evaluate_target_health = true
   }
