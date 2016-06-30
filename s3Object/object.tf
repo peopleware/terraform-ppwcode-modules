c34c52f6e2af6fce709cc0aba29fb9179fc89d17
Jan Dockx <Jan_Dockx@peopleware.be>
2016-06-30 14:39:47 +0200

    2 new modules to try out: making a public website with S3

---
+++ s3Object/object.tf
@@ -0,0 +1,44 @@
+/**
+ * An object in an S3 bucket, ready for publication over the internet.
+ *
+ * We require a directory in the main terraform project
+ *
+ *   resources/<BUCKET>/
+ *
+ * Inside this directory, files should be structured as you wish them in the S3 bucket.
+ * `key` should be relative to resources/<BUCKET>/. `key` is used as S3 key.
+ */
+
+variable "bucket" {
+  type = "string"
+}
+
+variable "key" {
+  type = "string"
+}
+
+variable "content-type" {
+  default = "text/html"
+}
+
+variable "cache-control" {
+  default = "no-cache"
+}
+
+variable "content-language" {
+  default = "en-US"
+}
+
+variable "RESOURCE_ROOT" {
+  default = "resources"
+}
+
+resource "aws_s3_bucket_object" "OBJECT" {
+  bucket = "${var.bucket}"
+  key = "${var.key}"
+  source = "${path.root}/${var.RESOURCE_ROOT}/${var.bucket}/${var.key}"
+  content_type = "${var.content-type}"
+  cache_control = "${var.cache-control}"
+  content_language = "${var.content-language}"
+  etag = "${md5(file("${path.root}/${var.RESOURCE_ROOT}/${var.bucket}/${var.key}"))}"
+}
---
+++ s3Site/s3site.tf
@@ -0,0 +1,56 @@
+/**
+ * An S3 bucket configured as a public site, accessible via a simple FQDN.
+ *
+ * The bucket name, and DNS-name of the public site, will be
+ *
+ * <short-name>.<domain>
+ *
+ * The domain name will be registered in the hosted zone with name `zone_id`.
+ * This should be the id of the hosted zone that manages `domain`.
+ */
+
+variable "short-name" {
+  type = "string"
+}
+
+variable "domain" {
+  type = "string"
+}
+
+variable "zone_id" {
+  type = "string"
+}
+
+resource "aws_s3_bucket" "BUCKET" {
+  bucket = "${var.short-name}.${var.domain}"
+  acl = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+
+  versioning {
+    enabled = false
+  }
+
+  policy = <<EOT
+{
+  "Id": "Policy1380877762691",
+  "Statement": [
+    {
+      "Sid": "Stmt1380877761162",
+      "Action": [
+        "s3:GetObject"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::${var.short-name}.${var.domain}/*",
+      "Principal": {
+        "AWS": [
+          "*"
+        ]
+      }
+    }
+  ]
+}
+EOT
+}
