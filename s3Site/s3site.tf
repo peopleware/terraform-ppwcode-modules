db8c807dc093ecdd1ed371d748140e83cb1dfcb5
Jan Dockx <Jan_Dockx@peopleware.be>
2016-06-30 16:23:25 +0200

    Fixing an error.
    (BTW: website_endpoint is the full FQDN)

--- s3Site/s3site.tf
+++ s3Site/s3site.tf
@@ -58,7 +58,7 @@ EOT
 resource "aws_route53_record" "DOMAIN_NAME" {
   zone_id = "${var.zone_id}"
   name = "${aws_s3_bucket.BUCKET.bucket}"
-  type = "ALIAS"
+  type = "A"
 
   alias {
     name = "${aws_s3_bucket.BUCKET.website_endpoint}"
