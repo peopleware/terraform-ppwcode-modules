83c85bb3fa314a4af6da28d0f5c89a3966e7131c
Jan Dockx <Jan_Dockx@peopleware.be>
2017-05-15 13:57:53 +0200

    Split of ns-record settings

--- subdomain/zone.tf
+++ subdomain/zone.tf
@@ -19,20 +19,3 @@ resource "aws_route53_zone" "ZONE" {
   tags {
   }
 }
-
-resource "aws_route53_record" "NS_DELEGATION" {
-  zone_id = "${var.parent-domain-zone_id}"
-  name = "${aws_route53_zone.ZONE.name}"
-  type = "NS"
-  records = [
-    "${aws_route53_zone.ZONE.name_servers.0}",
-    "${aws_route53_zone.ZONE.name_servers.1}",
-    "${aws_route53_zone.ZONE.name_servers.2}",
-    "${aws_route53_zone.ZONE.name_servers.3}"
-  ]
-  /* There seems to be no other way to communicate this list.
-     See example at
-     https://www.terraform.io/docs/providers/aws/r/route53_zone.html#name_servers */
-  ttl = "${var.ttl}"
-
-}
