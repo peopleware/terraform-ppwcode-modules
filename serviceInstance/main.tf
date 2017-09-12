33f6768b43085056fe516bd5f554e5781fd293d0
Jan Dockx <Jan_Dockx@peopleware.be>
2017-09-12 15:49:46 +0200

    define a serviceInstance module - untested

---
+++ serviceInstance/main.tf
@@ -0,0 +1,65 @@
+/**
+ *    Copyright 2017 PeopleWare n.v.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Define a service instance in the given hosted zone for the given domain
+ * according to DNS-SD RFC 6763 <https://www.ietf.org/rfc/rfc6763.txt>
+ *
+ * This defines
+ * - 1 PTR record that lists the service instance as an instance that offers the given service
+ * - 1 SRV record that defines at which domain name and port the service instance operates, and
+ *   with which priority and weight
+ * - 1 TXT record that lists specfics about the service instance
+ */
+
+locals {
+  type        = "_${trimspace(var.type)}._${trimspace(var.protocol)}.${trimspace(var.domain-name)}"
+  fullType    = "${trimspace(var.subtype) == "" ? "" : format("_%s._sub.", trimspace(var.subtype))}${local.type}"
+  instance    = "${trimspace(var.instance)}.${local.type}"
+  submitted   = "${timestamp()}"
+  fullDetails = "${merge(var.details, map("submitted", local.submitted))}"
+}
+
+resource "aws_route53_record" "ptr" {
+  zone_id = "${var.domain-zone_id}"
+  name    = "${local.fullType}"
+  type    = "PTR"
+  ttl     = "${var.ttl}"
+
+  records = [
+    "${local.instance}",
+  ]
+}
+
+resource "aws_route53_record" "srv" {
+  zone_id = "${var.domain-zone_id}"
+  name    = "${local.instance}"
+  type    = "SRV"
+  ttl     = "${var.ttl}"
+
+  records = [
+    "${format("%d %d %d %s", var.priority, var.weight, var.port, var.host)}",
+  ]
+}
+
+resource "aws_route53_record" "txt" {
+  zone_id = "${var.domain-zone_id}"
+  name    = "${local.instance}"
+  type    = "TXT"
+  ttl     = "${var.ttl}"
+
+  records = ["${formatlist("%s=%s", keys(local.fullDetails), values(local.fullDetails))}"]
+}
---
+++ serviceInstance/variables.tf
@@ -0,0 +1,136 @@
+/**
+ *    Copyright 2017 PeopleWare n.v.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+variable "domain-zone_id" {
+  type = "string"
+
+  description = <<EOF
+AWS zone id of the zone that controls the domain in which the service operates.
+EOF
+}
+
+variable "domain-name" {
+  type = "string"
+
+  description = <<EOF
+FQDN of the domain in which the service operates. This must be the domain controlled
+by the zone for which $${var.domain-zone_id} is given, or a subdomain of that domain.
+EOF
+}
+
+variable "protocol" {
+  type = "string"
+
+  description = <<EOF
+Protocol of the service. 'tcp' (default) or 'udp'.
+EOF
+
+  default = "tcp"
+}
+
+variable "type" {
+  type = "string"
+
+  description = <<EOF
+Service type identification. The starting dash is added automatically. Can be extended
+with an optional subtype. For APIs, use type "api", and specify the actual API with
+a subtype. For UIs, use type "http". A subtype is optional.
+EOF
+}
+
+variable "subtype" {
+  type = "string"
+
+  description = <<EOF
+Service subtype identification. The starting dash is added automatically. Optional.
+EOF
+
+  default = ""
+}
+
+variable "instance" {
+  type = "string"
+
+  description = <<EOF
+Name of the service instance. This should be human readable, and can contain any character
+in principle. Currently, the user must replace all readable ASCII characters by an 3-digit
+octal (double) escape sequence. E.g., instead of a space, use '\\040'.
+EOF
+}
+
+variable "host" {
+  type = "string"
+
+  description = <<EOF
+FQDN of where the service instance is hosted.
+EOF
+}
+
+variable "port" {
+  type = "string"
+
+  description = <<EOF
+Port at which the service instance is hosted at the $${var.host}. Default is 443 (https).
+EOF
+
+  default = "443"
+}
+
+variable "priority" {
+  type = "string"
+
+  description = <<EOF
+Priority of this service instance among it's peers. Default 0.
+EOF
+
+  default = "0"
+}
+
+variable "weight" {
+  type = "string"
+
+  description = <<EOF
+Weight of this service instance among it's peers with the same priority. Default 0.
+EOF
+
+  default = "0"
+}
+
+variable "details" {
+  type = "map"
+
+  description = <<EOF
+Details about the service instance. Should at least contain a value for the key 'textvers',
+containing a number that represents the data version of this record,
+and a value for the key 'at', containing an UTC timestamp string in RFC 3339 format that
+reflects when this information was defined. An UTC timestamp string in RFC 3339 format
+is added for the key 'submitted', that reflects when this information was last updated
+in DNS (which should be later than 'at'). You should avoid using keys 'type', 'name',
+'host', 'port', 'priority', 'weight'
+EOF
+
+  default = {}
+}
+
+variable "ttl" {
+  type = "string"
+
+  description = <<EOF
+TTL in s of the PTR, SRV and TXT records.
+EOF
+
+  default = "30"
+}
