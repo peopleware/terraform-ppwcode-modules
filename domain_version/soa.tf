f0265baaa4493f3cbc7ddf2fa1af52da042a36cf
Jan Dockx <Jan_Dockx@peopleware.be>
2018-11-05 17:28:06 +0100

    Fix bug introduced with terraform 0.11.10
    Related to  https://github.com/terraform-providers/terraform-provider-aws/issues/241
    
    With different terraform versions, zone.name will have a trailing dot, or not.

--- domain_version/soa.tf
+++ domain_version/soa.tf
@@ -32,8 +32,11 @@ resource "aws_route53_record" "soa" {
   name    = "${var.domain_name}"
   type    = "SOA"
 
+  # NOTE: Because of recurring issues with a zone.name having a trailing dot, yes or no, just make sure it is removed if
+  # it occurs
+  # https://github.com/terraform-providers/terraform-provider-aws/issues/241
   records = [
-    "${var.ns-domain_name}. hostmaster.${var.domain_name}. ${data.external.calculated_meta.result.serial} 7200 900 1209600 86400",
+    "${var.ns-domain_name}. hostmaster.${replace(var.domain_name, "/\\.$/", "")}. ${data.external.calculated_meta.result.serial} 7200 900 1209600 86400",
   ]
 
   ttl = "${var.ttl}"
