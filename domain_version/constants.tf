f4f28dd8f06847f13ab3fbb2ac10de0b21372e5f
Jan Dockx <Jan_Dockx@peopleware.be>
2020-05-15 13:02:27 +0200

    remove domain_version

--- domain_version/constants.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  # NOTE: Because of recurring issues with a zone.name having a trailing dot, yes or no, just make sure it is removed if
-  # it occurs
-  # https://github.com/terraform-providers/terraform-provider-aws/issues/241
-  clean_domain_name = "${replace(var.domain_name, "/\\.$/", "")}"
-}
